b80858aa9cbcaf0b34491db62194e41637e73431
Benjamin Catalano <Benjamin.Catalano@mines.sdsmt.edu>
2021-10-01 09:05:19 -0600

    Added Vigenere attack

--- inc/main.h
+++ inc/main.h
@@ -5,12 +5,16 @@
 #include <stdio.h>
 #include <iostream>
 #include <limits>
+#include <vector>
+#include <deque>
 #include "utility.h"
 #include "affinecipher.h"
 #include "vigenerecipher.h"
 
 using std::cout;
 using std::cin;
+using std::deque;
+using std::vector;
 
 constexpr const char * const MENU =
     "\n--- Menu ---\n"
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "main.h"
 
 
+bool affine_attack();
 bool affine_menu();
 void frequency_menu();
+bool vigenere_attack();
 bool vigenere_menu();
 
 
@@ -27,11 +29,13 @@ int main() {
                 break;
 
             case 4: // Affine Cipher attack
-                printf("Frequency calculation\n");
+                if ( !affine_attack() )
+                    fputs("Bad input: returning to main menu...\n", stdout);
                 break;
 
             case 5: // Vigenère Cipher attack
-                printf("Vigenère Cipher attack\n");
+                if ( !vigenere_attack() )
+                    fputs("Bad input: returning to main menu...\n", stdout);
                 break;
 
             case 6: // ADFGX Cipher
@@ -50,6 +54,42 @@ int main() {
     }
 
     return 0;
+}
+
+
+bool affine_attack() {
+    char p1, p2, c1, c2;
+    fputs("Four characters are needed--'p1', 'p2', 'c1', 'c2'--"
+          "where 'pi' is plaintext and 'ci' is ciphertext s.t.:\n"
+          "\tE(p1)=c1 and E(p2)=c2\n"
+          "Enter p1: ", stdout);
+    if ( !( cin >> p1 ) )
+        return false;
+    fputs("Enter c1: ", stdout);
+    if ( !( cin >> c1 ) )
+        return false;
+    fputs("Enter p2: ", stdout);
+    if ( !( cin >> p2 ) )
+        return false;
+    fputs("Enter c2: ", stdout);
+    if ( !( cin >> c2 ) )
+        return false;
+
+    printf("\nGiven: %c->%c; %c->%c\n", p1, c1, p2, c2);
+    // a * p1 + b = c1; a * p2 + b = c2
+    // a = (c1 - c2) * (p1 - p2)^-1
+    // b = c1 - a * p1
+    Int x1 = ord(toupper(p1));
+    Int x2 = ord(toupper(p2));
+    Int y1 = ord(toupper(c1));
+    Int y2 = ord(toupper(c2));
+
+    Int a = mod((y1 - y2) * findModInverse(x1 - x2, 26), 26);
+    Int b = mod(y1 - a * x1, 26);
+
+    printf("The affine cipher is:\n\tE(mi) = %d * mi + %d (mod 26)\n", a, b);
+
+    return true;
 }
 
 
@@ -102,6 +142,76 @@ void frequency_menu() {
         printf("   '%c'    :   %.4f\n", p.first, p.second);
 
     cout << std::endl;
+}
+
+
+bool vigenere_attack() {
+    string cipher_text;
+    int key_length = 1;
+
+    fputs("Enter the cipher text: ", stdout);
+    cin.clear(); cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    getline(cin, cipher_text);
+    cipher_text = to_alphabet(toupper(cipher_text), ALPHABET);
+
+    int max_coincidence = 0;
+    for ( unsigned i = 1; i < cipher_text.size(); ++i ) {
+        int coincidence = 0;
+        unsigned k = i;
+        for ( int j = 0; k < cipher_text.size(); ++j, ++k ) {
+            if ( cipher_text[j] == cipher_text[k] )
+                ++coincidence;
+        }
+        if ( coincidence > max_coincidence ) {
+            max_coincidence = coincidence;
+            key_length = i;
+        }
+    }
+    printf("key length: %d\n", key_length);
+    vector<int> key;
+    key.reserve(key_length);
+
+    vector<float> w;
+    w.reserve(26);
+
+    auto a = deque<float>(STANDARD_FREQUENCIES, STANDARD_FREQUENCIES + 26);
+    for ( int i = 0; i < key_length; ++i ) {
+        w.clear();
+        for ( int j = 0; j < 26; ++j ) {
+            int count = 0;
+            for ( unsigned k = i; k < cipher_text.size(); k += key_length ) {
+                if ( cipher_text[k] == chr(j) )
+                    ++count;
+            }
+            w.push_back(float(count) / cipher_text.size());
+        }
+
+        float max_dot = 0.0;
+        int shift = 0;
+
+        for ( int j = 0; j < 26; ++j ) {
+            float dot = 0;
+            for ( int k = 0; k < 26; ++k )
+                dot += w[k] * a[k];
+
+            if ( dot > max_dot ) {
+                max_dot = dot;
+                shift = j;
+            }
+
+            a.push_front(a.back());
+            a.pop_back();
+        }
+
+        key.push_back(shift);
+    }
+
+    fputs("The key is probably:\n\t", stdout);
+    for ( int i = 0; i < key_length; ++i )
+        cout << chr(key[i]);
+    cout << std::endl;
+
+    return true;
 }
 
 
